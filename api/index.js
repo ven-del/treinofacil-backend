8adea2e690cc48e6872a0174a8f0ceaa60142355
Wendell Régis <r.wendell.regis@gmail.com>
2025-06-19 09:04:55 -0300

    Update index.js
    Adicionado CORS para liberar o consumo da API.

--- api/index.js
+++ api/index.js
@@ -7,7 +7,15 @@ const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000", // Local
+      "https://labce-treinofacil-backend.vercel.app", // Produção
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api", publicRoutes);
